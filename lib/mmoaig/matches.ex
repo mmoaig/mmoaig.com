98c8c9a2a8287095bf1d3da2fd38137bfef5baab
Ryan Morris <mryan.morris@gmail.com>
2024-02-03 18:26:15 +0000

    feat(training-matches): broadcast match updates

--- lib/mmoaig/matches.ex
+++ lib/mmoaig/matches.ex
@@ -8,6 +8,7 @@ defmodule Mmoaig.Matches do
 
   alias Mmoaig.Matches.Match
   alias Mmoaig.Matches.Runner
+  alias Mmoaig.Matches.Updates
 
   @doc """
   Returns the list of matches.
@@ -72,6 +73,7 @@ defmodule Mmoaig.Matches do
     match
     |> Match.changeset(attrs)
     |> Repo.update()
+    |> broadcast_match_update()
   end
 
   @doc """
@@ -106,4 +108,13 @@ defmodule Mmoaig.Matches do
   def start_runner(match) do
     DynamicSupervisor.start_child(Mmoaig.Matches.Runner.Supervisor, {Runner, match})
   end
+
+  def subscribe_to_match_updates(match_id), do: Updates.subscribe(match_id)
+
+  defp broadcast_match_update({:ok, match}) do
+    Updates.broadcast(match)
+    {:ok, match}
+  end
+
+  defp broadcast_match_update(match), do: match
 end
