3139b2fd6ffc68be99d5494644a02b99d7a2a159
Ryan Morris <mryan.morris@gmail.com>
2024-02-11 10:32:35 +0000

    feat(matches): start new matches when they are created

--- lib/mmoaig/matches/runner.ex
+++ lib/mmoaig/matches/runner.ex
@@ -3,7 +3,6 @@ defmodule Mmoaig.Matches.Runner do
 
   alias Mmoaig.Matches
 
-  @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(match) do
     GenServer.start_link(__MODULE__, match, name: __MODULE__)
   end
@@ -15,12 +14,14 @@ defmodule Mmoaig.Matches.Runner do
 
   def handle_info(:start_match, match) do
     {:ok, match} = Matches.update_match(match, %{status: "in-progress"})
+    Matches.create_log_message("info", %{match_id: match.id, message: "Match started"})
     Process.send_after(self(), :complete_match, 10_000)
     {:noreply, match}
   end
 
   def handle_info(:complete_match, match) do
     {:ok, match} = Matches.update_match(match, %{status: "complete"})
+    Matches.create_log_message("info", %{match_id: match.id, message: "Match completed"})
     Process.send_after(self(), :stop, 1_000)
     {:noreply, match}
   end
